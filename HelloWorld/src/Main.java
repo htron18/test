7deeaa14fd8f44a26700448fbbb2374393ea7292
sugataku1208 <htron405@gmail.com>
2018-03-28 17:54:17 +0900

    3/28

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,19 @@
+/**
+ *
+ */
+
+/**
+ * @author internousdev
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args){
+		String s="123";
+		int i=Integer.parseInt(s);
+		System.out.println(s);
+	}
+}
